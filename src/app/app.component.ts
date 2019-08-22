609c1f3e4db337dbd5d21ec90bc0191007ac75b2
tienanhcntt2 <VNW0013736@vn.fpg.com>
2019-08-22 14:56:31 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoHuamian';
+
+  customizeText(arg: any) {
+    let temp = '';
+    switch (arg.valueText) {
+      case '0' || '360': {
+        //statements; 
+        temp = 'N';
+        break;
+      }
+      
+      
+      case '45.0': {
+        //statements; 
+        temp = 'NE';
+        break;
+      }
+      case '90.0': {
+        //statements; 
+        temp = 'E';
+        break;
+      }
+      case '135.0': {
+        //statements; 
+        temp = 'SE';
+        break;
+      }
+      case '180.0': {
+        //statements; 
+        temp = 'S';
+        break;
+      }
+      case '225.0': {
+        //statements; 
+        temp = 'SW';
+        break;
+      }
+      case '270.0': {
+        //statements; 
+        temp = 'W';
+        break;
+      }
+      case '315.0': {
+        //statements; 
+        temp = 'WN';
+        break;
+      }
+      default: {
+        //statements; 
+        break;
+      }
+    }return temp;
+  }
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DxCircularGaugeModule,DxLinearGaugeModule } from 'devextreme-angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    DxCircularGaugeModule,
+    DxLinearGaugeModule,
     AppRoutingModule
   ],
   providers: [],
